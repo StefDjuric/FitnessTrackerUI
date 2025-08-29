ef0c2384d5cfcee1a38f96b223835ccffc50879c
Stefan <djuric.stef4n@gmail.com>
2025-08-29 11:54:27 +0200

    Add programs download

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -14,12 +14,33 @@ export class Button {
   @Input() styling: 'primary' | 'secondary' | 'tertiary' = 'primary';
   @Input() disabled: boolean = false;
   @Input() iconSrc: string | null = null;
+  @Input() downloadUrl?: string;
+  @Input() downloadFileName?: string;
 
   @Output() clicked = new EventEmitter<MouseEvent>();
 
   OnClick(event: MouseEvent) {
     if (!this.disabled) {
+      if (this.downloadUrl) {
+        this.downloadFromUrl();
+      }
+
       this.clicked.emit(event);
     }
+  }
+
+  private downloadFromUrl() {
+    if (!this.downloadFileName) return;
+
+    const link = document.createElement('a');
+    if (this.downloadUrl) link.href = this.downloadUrl;
+
+    if (this.downloadFileName) link.download = this.downloadFileName;
+
+    link.style.display = 'none';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.appendChild(link);
   }
 }
